df764a24ce8534a286778825a29ca141198aeab8
pulasthilakshan <pulasthilakshan@gmail.com>
2015-07-03 16:56:11 +0530

    teams inner page started

---
+++ players/team.php
@@ -0,0 +1,215 @@
+<html>
+<head>
+	
+	<title>Team</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="js/woothemes-FlexSlider-8ed61ac/flexslider.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="js/woothemes-FlexSlider-8ed61ac/jquery.flexslider.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" charset="utf-8">
+	  $(window).load(function() {
+	    $('#flexslider-carousel').flexslider({
+	    	animation: "slide",
+	    	animationLoop: true,
+	    	slideshow: true,
+	    	controlNav: false,
+	    	prevText: "",
+	    	nextText: "",
+	    	itemWidth: 800,
+	    	itemMargin: 0,
+	    	minItems: 5,
+	    	move: 1
+	    });
+	   });
+	</script>
+
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+</head>
+<body>
+	<div id="fb-root"></div>
+	<div id="fb-root"></div>
+	<header class="header">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="leaderboard">
+			<a href="#">
+				<div class="abc"></div>
+				<img src="photos/logo/leaderboard.jpg">
+			</a>
+		</div>
+	</header>
+	<nav class="navigation">
+		<ul class="nav-links">
+			<a href="index.php"><li>Home</li></a>
+			<a href="players.php" style="color:#F9C700" style="background-color: #333; border-top-left-radius: 3px; border-top-right-radius: 3px;"><li>Players</li></a>
+			<a href="news.php"><li>News</li></a>
+			<a href="blogs.php"><li>Blogs</li></a>
+			<a href="guides.php"><li>Guides</li></a>
+			<a href=""><li>Videos</li></a>
+			<a href=""><li>Streams</li></a>
+			<a href="matches.php"><li>Matches</li></a>
+			<a href="galleries.php"><li>Galleries</li></a>
+			<a href="forums.php"><li>Forums</li></a>
+			<a href="downloads.php"><li>Downloads</li></a>
+			<a href="about.php"><li>About</li></a>
+		</ul>
+		<ul class="social">
+			<li class="anim-01 twitch"><a href="http://www.twitch.tv/team/dignitas">
+				<i class="fa fa-twitch"></i> 
+			</a></li>
+			<li class="anim-01 twitter"><a href="http://www.twitter.com">
+				<i class="fa fa-twitter"></i> 
+			</a></li>
+			<li class="anim-01 facebook"><a href="http://www.facebook.com">
+				<i class="fa fa-facebook"></i> 
+			</a></li>
+			<li class="anim-01 youtube"><a href="http://www.youtube.com">
+				<i class="fa fa-youtube"></i> 
+			</a></li>
+			<li class="anim-01 steam"><a href="http://www.steam.com">
+				<i class="fa fa-steam"></i> 
+			</a></li>
+			<li class="anim-01 google-plus"><a href="https://plus.google.com">
+				<i class="fa fa-google-plus"></i> 
+			</a></li>
+		</ul>
+	</nav>
+	<div class="sponsors">
+		<div class="flexslider" id="flexslider-carousel" style="height: 100px">
+		  <ul class="slides">
+		    <li style="background:url(photos/sponsors/diesel-logo.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-puma.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Levis-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Aeropostale-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-alaskan.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		  </ul>
+		</div>
+	</div>
+	<div class="wrapper-content">
+		<div class="teams-area">
+			<h2 class="heading">COD</h2>
+			<div class="teams-cover">
+				<img src="photos/players/team1/cover.jpg">
+			</div>	
+			<ul class="team">
+				<li class="team-member">
+					<div class="pic-border">
+						<div class="pic-container" style="background: url(photos/galleries/gallery1/pic1.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+					</div>
+				</li>
+				<li class="team-member">
+					<div class="pic-border">
+						<div class="pic-container" style="background: url(photos/galleries/gallery1/pic2.png); background-size: cover; background-repeat: no-repeat;"></div>
+					</div>
+				</li>
+				<li class="team-member">
+					<div class="pic-border">
+						<div class="pic-container" style="background: url(photos/galleries/gallery1/pic3.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+					</div>
+				</li>
+				<li class="team-member">
+					<div class="pic-border">
+						<div class="pic-container" style="background: url(photos/galleries/gallery1/pic4.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+					</div>
+				</li>
+				<li class="team-member">
+					<div class="pic-border">
+						<div class="pic-container" style="background: url(photos/galleries/gallery1/pic5.jpg); background-size: cover; background-repeat: no-repeat;"></div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="main-footer">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="legal">
+			<h3>Legal</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+		<div class="site-map">
+			<h3>Site Map</h3>
+			<div class="links">
+				<ul class="set-1">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Blogs</a>
+					</li>
+					<li>
+						<a href="#">Videos</a>
+					</li>
+					<li>
+						<a href="#">Galleries</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+				</ul>
+				<ul class="set-2">
+					<li>
+						<a href="#">Players</a>
+					</li>
+					<li>
+						<a href="#">Guides</a>
+					</li>
+					<li>
+						<a href="#">Streams</a>
+					</li>
+					<li>
+						<a href="#">Forums</a>
+					</li>
+				</ul>
+				<ul class="set-3">
+					<li>
+						<a href="#">News</a>
+					</li>
+					<li>
+						<a href="#">Shop</a>
+					</li>
+					<li>
+						<a href="#">Matches</a>
+					</li>
+					<li>
+						<a href="#">Downloads</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="about">
+			<h3>About</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+	</div>
+	<div id="logo-footer">
+		<span>N00b Alliance</span>
+		<div style="float:right">
+			<span>Powered by</span>
+			<span id="comp">Ingens</span>
+		</div>
+	</div>
+</body>
+</html>
---
+++ team.php
@@ -0,0 +1,221 @@
+<html>
+<head>
+	
+	<title>Team</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="js/woothemes-FlexSlider-8ed61ac/flexslider.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="js/woothemes-FlexSlider-8ed61ac/jquery.flexslider.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" charset="utf-8">
+	  $(window).load(function() {
+	    $('#flexslider-carousel').flexslider({
+	    	animation: "slide",
+	    	animationLoop: true,
+	    	slideshow: true,
+	    	controlNav: false,
+	    	prevText: "",
+	    	nextText: "",
+	    	itemWidth: 800,
+	    	itemMargin: 0,
+	    	minItems: 5,
+	    	move: 1
+	    });
+	   });
+	</script>
+
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+</head>
+<body>
+	<div id="fb-root"></div>
+	<div id="fb-root"></div>
+	<header class="header">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="leaderboard">
+			<a href="#">
+				<div class="abc"></div>
+				<img src="photos/logo/leaderboard.jpg">
+			</a>
+		</div>
+	</header>
+	<nav class="navigation">
+		<ul class="nav-links">
+			<a href="index.php"><li>Home</li></a>
+			<a href="players.php" style="color:#F9C700" style="background-color: #333; border-top-left-radius: 3px; border-top-right-radius: 3px;"><li>Players</li></a>
+			<a href="news.php"><li>News</li></a>
+			<a href="blogs.php"><li>Blogs</li></a>
+			<a href="guides.php"><li>Guides</li></a>
+			<a href=""><li>Videos</li></a>
+			<a href=""><li>Streams</li></a>
+			<a href="matches.php"><li>Matches</li></a>
+			<a href="galleries.php"><li>Galleries</li></a>
+			<a href="forums.php"><li>Forums</li></a>
+			<a href="downloads.php"><li>Downloads</li></a>
+			<a href="about.php"><li>About</li></a>
+		</ul>
+		<ul class="social">
+			<li class="anim-01 twitch"><a href="http://www.twitch.tv/team/dignitas">
+				<i class="fa fa-twitch"></i> 
+			</a></li>
+			<li class="anim-01 twitter"><a href="http://www.twitter.com">
+				<i class="fa fa-twitter"></i> 
+			</a></li>
+			<li class="anim-01 facebook"><a href="http://www.facebook.com">
+				<i class="fa fa-facebook"></i> 
+			</a></li>
+			<li class="anim-01 youtube"><a href="http://www.youtube.com">
+				<i class="fa fa-youtube"></i> 
+			</a></li>
+			<li class="anim-01 steam"><a href="http://www.steam.com">
+				<i class="fa fa-steam"></i> 
+			</a></li>
+			<li class="anim-01 google-plus"><a href="https://plus.google.com">
+				<i class="fa fa-google-plus"></i> 
+			</a></li>
+		</ul>
+	</nav>
+	<div class="sponsors">
+		<div class="flexslider" id="flexslider-carousel" style="height: 100px">
+		  <ul class="slides">
+		    <li style="background:url(photos/sponsors/diesel-logo.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-puma.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Levis-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Aeropostale-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-alaskan.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		  </ul>
+		</div>
+	</div>
+	<div class="wrapper-content">
+		<div class="guides-area">
+			<h2 class="heading">n00b Alliance PLAYER GUIDES</h2>
+			<div class="guides-cover">
+				<img src="photos/teams/team1.jpg">
+			</div>	
+			<ul class="guides">
+				<li>
+					<div class="guides-des">
+						<p>Welcome to the n00b Alliance Player Guides!</p>
+						<p>These guides will help you understand the essentials of your game and serve as "cheat sheets" for you to quickly access during your games. Unlike most other guide websites, these guides are created by world class professional gamers only, so you will get your information from the best possible sources.</p>
+						<h2>First, make your choice!</h2>
+					</div>
+				</li>
+				<li>
+					<div class="guide">
+						<div>
+							<a href="#"><h2 class="guide-heading">League of Legends</h2></a>
+							<a href="#"><img src="photos/teams/team3.jpg"></a>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="guide">
+						<div>
+							<a href="#"><h2 class="guide-heading">StarCraft II</h2></a>
+							<a href="#"><img src="photos/teams/team4.png"></a>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="guide">
+						<div>
+							<a href="#"><h2 class="guide-heading">SMITE</h2></a>
+							<a href="#"><img src="photos/teams/team5.png"></a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="main-footer">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="legal">
+			<h3>Legal</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+		<div class="site-map">
+			<h3>Site Map</h3>
+			<div class="links">
+				<ul class="set-1">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Blogs</a>
+					</li>
+					<li>
+						<a href="#">Videos</a>
+					</li>
+					<li>
+						<a href="#">Galleries</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+				</ul>
+				<ul class="set-2">
+					<li>
+						<a href="#">Players</a>
+					</li>
+					<li>
+						<a href="#">Guides</a>
+					</li>
+					<li>
+						<a href="#">Streams</a>
+					</li>
+					<li>
+						<a href="#">Forums</a>
+					</li>
+				</ul>
+				<ul class="set-3">
+					<li>
+						<a href="#">News</a>
+					</li>
+					<li>
+						<a href="#">Shop</a>
+					</li>
+					<li>
+						<a href="#">Matches</a>
+					</li>
+					<li>
+						<a href="#">Downloads</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="about">
+			<h3>About</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+	</div>
+	<div id="logo-footer">
+		<span>N00b Alliance</span>
+		<div style="float:right">
+			<span>Powered by</span>
+			<span id="comp">Ingens</span>
+		</div>
+	</div>
+</body>
+</html>
