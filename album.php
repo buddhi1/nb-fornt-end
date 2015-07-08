85ba841067747002db4a6e80a0d5f8c09a9c9180
pulasthilakshan <pulasthilakshan@gmail.com>
2015-07-08 15:14:49 +0530

    gallery done

---
+++ album.php
@@ -0,0 +1,344 @@
+<html>
+<head>
+	
+	<title>Galleries</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="js/woothemes-FlexSlider-8ed61ac/flexslider.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="js/woothemes-FlexSlider-8ed61ac/jquery.flexslider.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,800,400' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" charset="utf-8">
+	  $(window).load(function() {
+	    $('#flexslider-carousel').flexslider({
+	    	animation: "slide",
+	    	animationLoop: true,
+	    	slideshow: true,
+	    	controlNav: false,
+	    	prevText: "",
+	    	nextText: "",
+	    	itemWidth: 800,
+	    	itemMargin: 0,
+	    	minItems: 5,
+	    	move: 1
+	    });
+	   });
+	</script>
+
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.css" />
+	<style type="text/css">
+	    a.fancybox img {
+	        border: none;
+	        box-shadow: 0 1px 7px rgba(0,0,0,0.6);
+	        -o-transform: scale(1,1); -ms-transform: scale(1,1); -moz-transform: scale(1,1); -webkit-transform: scale(1,1); transform: scale(1,1); -o-transition: all 0.2s ease-in-out; -ms-transition: all 0.2s ease-in-out; -moz-transition: all 0.2s ease-in-out; -webkit-transition: all 0.2s ease-in-out; transition: all 0.2s ease-in-out;
+	    } 
+	    a.fancybox:hover img {
+	        position: relative; z-index: 999; -o-transform: scale(1.03,1.03); -ms-transform: scale(1.03,1.03); -moz-transform: scale(1.03,1.03); -webkit-transform: scale(1.03,1.03); transform: scale(1.03,1.03);
+	    }
+	</style>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<div id="fb-root"></div>
+	<header class="header">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="leaderboard">
+			<a href="#">
+				<div class="abc"></div>
+				<img src="photos/logo/leaderboard.jpg">
+			</a>
+		</div>
+	</header>
+	<nav class="navigation">
+		<ul class="nav-links">
+			<a href="index.php"><li>Home</li></a>
+			<a href="players.php"><li>Players</li></a>
+			<a href="news.php"><li>News</li></a>
+			<a href="blogs.php"><li>Blogs</li></a>
+			<a href=""><li>Videos</li></a>
+			<a href=""><li>Streams</li></a>
+			<a href="matches.php"><li>Matches</li></a>
+			<a href="galleries.php"><li class="active">Galleries</li></a>
+			<a href="forums.php"><li>Forums</li></a>
+			<a href="downloads.php"><li>Downloads</li></a>
+			<a href="about.php"><li>About</li></a>
+			<a href="contact.php"><li>Contact</li></a>
+		</ul>
+		<ul class="social">
+			<li class="anim-01 twitch"><a href="http://www.twitch.tv/team/dignitas">
+				<i class="fa fa-twitch"></i> 
+			</a></li>
+			<li class="anim-01 twitter"><a href="http://www.twitter.com">
+				<i class="fa fa-twitter"></i> 
+			</a></li>
+			<li class="anim-01 facebook"><a href="http://www.facebook.com">
+				<i class="fa fa-facebook"></i> 
+			</a></li>
+			<li class="anim-01 youtube"><a href="http://www.youtube.com">
+				<i class="fa fa-youtube"></i> 
+			</a></li>
+			<li class="anim-01 steam"><a href="http://www.steam.com">
+				<i class="fa fa-steam"></i> 
+			</a></li>
+			<li class="anim-01 google-plus"><a href="https://plus.google.com">
+				<i class="fa fa-google-plus"></i> 
+			</a></li>
+		</ul>
+	</nav>
+	<div class="sponsors">
+		<div class="flexslider" id="flexslider-carousel" style="height: 100px">
+		  <ul class="slides">
+		    <li style="background:url(photos/sponsors/diesel-logo.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-puma.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Levis-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images.png); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/Aeropostale-Company-Logo-Image.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		    <li style="background:url(photos/sponsors/images-alaskan.jpg); background-size: 240px 80px; background-repeat: no-repeat; display: inline-block;">
+		    	<a href="#"></a>
+		    </li>
+		  </ul>
+		</div>
+	</div>
+	<div class="wrapper-content">
+		<div class="album-area">
+			<div class="album-header">
+				<h2 class="heading">ESPORTS FESTIVAL 2015</h2>
+				<h2 class="photo-count">112 PICS</h2>
+			</div>
+			<ul class="album">
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li class="album-li">
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic1.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<li class="album-li">
+					<div class="pic-border">
+						<div class="pic-container">
+							<img class="fancybox" src="photos/galleries/gallery1/pic4.jpg">
+						</div>
+					</div>
+				</li>
+				<div class="album-description">
+					<p class="date">Posted Sat 4th Jul 2015 - 11:55pm</p>
+					<p class="des">Esports Festival 2015</p>
+				</div>
+				<div class="comment-container">
+					<h2 class="comment-heading">Your comments</h2>
+					<ul class="comment-ul">
+						<li>
+							<div class="author-info">
+								<a href=""><div class="author-image" style="background: url('photos/author/IMG_8778.JPG');"></div></a>
+								<a href=""><h2 class="author-name">MepH</h2></a>
+							</div>
+							<div class="comment-info">
+								<p class="comment-date">Sun 5th Jul 2015 - 4:08am</p>
+								<p class="comment">Excellent work today, boys! I look forward to another solid match tomorrow.</p>
+							</div>
+						</li>
+					</ul>
+					<p class="login-info">Please <a href="">register</a> or <a href="">login</a> to post comments</p>
+				</div>
+			</ul>
+			
+		</div>
+		
+	</div>
+	<div id="main-footer">
+		<div class="logo">
+			<img src="photos/logo/main-logo.png">
+		</div>
+		<div class="legal">
+			<h3>Legal</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+		<div class="site-map">
+			<h3>Site Map</h3>
+			<div class="links">
+				<ul class="set-1">
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Blogs</a>
+					</li>
+					<li>
+						<a href="#">Videos</a>
+					</li>
+					<li>
+						<a href="#">Galleries</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+				</ul>
+				<ul class="set-2">
+					<li>
+						<a href="#">Players</a>
+					</li>
+					<li>
+						<a href="#">Guides</a>
+					</li>
+					<li>
+						<a href="#">Streams</a>
+					</li>
+					<li>
+						<a href="#">Forums</a>
+					</li>
+				</ul>
+				<ul class="set-3">
+					<li>
+						<a href="#">News</a>
+					</li>
+					<li>
+						<a href="#">Shop</a>
+					</li>
+					<li>
+						<a href="#">Matches</a>
+					</li>
+					<li>
+						<a href="#">Downloads</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="about">
+			<h3>About</h3>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+		</div>
+	</div>
+	<div id="logo-footer">
+		<span>N00b Alliance</span>
+		<div style="float:right">
+			<span>Powered by</span>
+			<span id="comp">Ingens</span>
+		</div>
+	</div>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.pack.min.js"></script>
+	<script type="text/javascript">
+	    $(function($){
+	        var addToAll = false;
+	        var gallery = false;
+	        var titlePosition = 'inside';
+	        $(addToAll ? 'img' : 'img.fancybox').each(function(){
+	            var $this = $(this);
+	            var title = $this.attr('title');
+	            var src = $this.attr('data-big') || $this.attr('src');
+	            var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+	            $this.wrap(a);
+	        });
+	        if (gallery)
+	            $('a.fancybox').attr('rel', 'fancyboxgallery');
+	        $('a.fancybox').fancybox({
+	            titlePosition: titlePosition
+	        });
+	    });
+	    $.noConflict();
+	</script>
+</body>
+</html>
